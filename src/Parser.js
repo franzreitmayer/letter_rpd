0c8327fdd7d0dbf5cf5959c25f826418eabbda5c
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-07-07 21:04:44 +0200

    first parser steps and tokenizer

--- src/Parser.js
+++ src/Parser.js
@@ -1,10 +1,21 @@
+const { Tokenizer } = require('./Tokenizer.js');
+
 class Parser {
+
+    constructor() {
+        this._string = '';
+        this._tokenizer = new Tokenizer();
+    }
+
     /**
      * Parse a string to AST
      * @param {} string 
      */
     parse(string) {
         this._string = string;
+        this._tokenizer.init(this._string);
+
+        this._lookahead = this._tokenizer.getNextToken();
         return this.Program();
     }
 
@@ -13,14 +24,35 @@ class Parser {
      * @returns 
      */
     Program() {
-        return this.NumericalLiteral();
+        return {
+            type: 'Program',
+            body: this.NumericalLiteral()
+        }
     }
 
     NumericalLiteral() {
+        const token = this._eat('NUMBER');
         return {
             type: 'NumericalLiteral',
-            value: Number(this._string)
+            value: Number(token.value)
         }
+    }
+
+    _eat(tokenType) {
+        const token = this._lookahead;
+        if (token == null) {
+            throw new SyntaxError(`Unexpected end of input, expected ${tokenTyp}`);
+        }
+
+        if (token.type !== tokenType) {
+            throw new SyntaxError(
+                `Unexpected token: "${token.type}", expected "${tokenType}"`
+            );
+        }
+
+        this._lookahead = this._tokenizer.getNextToken();
+
+        return token;
     }
 }
 
---
+++ src/Tokenizer.js
@@ -0,0 +1,59 @@
+const Spec = [{
+        regExPattern: /^\d+/,
+        tokenType: 'NUMBER'
+    },
+    {
+        regExPattern: /^"[^"]*"/,
+        tokenType: 'STRING'
+    },
+    {
+        regExPattern: /^'[^']*'/,
+        tokenType: 'STRING'
+    }
+];
+
+
+class Tokenizer {
+    init(string) {
+        this._string = string;
+        this._cursor = 0;
+    }
+
+    hasMoreTokens() {
+        return this._cursor < this._string.length;
+    }
+
+
+    getNextToken() {
+        if (!this.hasMoreTokens()) {
+
+            return null;
+        }
+
+        for (const { regExPattern, tokenType }
+            of Spec) {
+            // const [regExPattern, tokenType] = Spec[i];
+            console.log(`regExPattern: ${regExPattern}, tokenType: ${tokenType}`);
+        }
+
+        const string = this._string.slice(this._cursor);
+
+        if (!Number.isNaN(Number(string[0]))) {
+            let number = '';
+            while (!(Number.isNaN(Number(string[this._cursor])))) {
+                number += string[this._cursor]
+                this._cursor++;
+            }
+            return {
+                type: 'NUMBER',
+                value: number
+            }
+        }
+
+
+    }
+}
+
+module.exports = {
+    Tokenizer
+}
